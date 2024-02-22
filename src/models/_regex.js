667a433ff86d47794fe59346257cb1e04c584b04
1-17 <dezessetedoonze@gmail.com>
2024-02-22 15:05:27 -0300

    Removed upperCaseLetters regex.

--- src/models/_regex.js
+++ src/models/_regex.js
@@ -2,8 +2,7 @@ const regex = {
   username: /^[a-zA-Z0-9._]+$/,
   numbersOnly: /^\d+$/,
   consecutiveDots: /\.{2,}/,
-  link: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/igm,
-  uppercaseLetters: /[A-Z]/
+  link: /[(http(s)?):\/\/(www\.)?a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/igm
 }
 
 export default regex
